1cee4a1103faec233190fd5916d893e895cbdd85
Vladislav Fomagin <fomagin1@yandex.ru>
2024-07-16 17:23:33 +0300

    module11-task1 first try not ready

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-import './gallery.js';
 import './form.js';
+import { renderGallery } from './gallery.js';
+import { getData } from './api.js';
+import { showAlert } from './message.js';
 
+getData()
+  .then((pictureList) => {
+    renderGallery(pictureList);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
